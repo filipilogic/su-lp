463902ede3c7845a74423101d457cae345dcef60
filipilogic <filip@lemon-mss.com>
2023-09-12 12:17:16 +0200

    fix

--- assets/public/js/frontend.js
+++ assets/public/js/frontend.js
@@ -14,6 +14,14 @@ jQuery(document).ready(function ($) {
     $("#primary-menu").fadeToggle();
     $(this).toggleClass('menu-open');
   });
+  $("#primary-menu li").click(function () {
+    var windowsize = $(window).width();
+    if (windowsize < 1200) {
+      $("#primary-menu").fadeToggle();
+      $(".menu-toggle").toggleClass('menu-open');
+    }
+  });
+
   // Sub Menu Trigger
 
   $("#primary-menu li.menu-item-has-children > a").after('<span class="sub-menu-trigger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg></span>');
